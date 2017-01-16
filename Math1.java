4ab2b07ebb63bbd9cd52aa1d272359a9c1a808d1
Kei Kimura <kimuchan0811@gmail.com>
2017-01-16 15:44:22 +0900

    Update Math1.java

--- Math1.java
+++ Math1.java
@@ -1,6 +1,9 @@
 
 class Math1 {
+	//sinのクラスメソッド
 	static double sin(double x){ return Math.sin(Math.PI*(x)); }
+	//cosのクラスメソッド
 	static double cos(double x){ return Math.cos(Math.PI*(x)); }
+	//tanのクラスメソッド
 	static double tan(double x){ return Math.tan(Math.PI*(x)); }
 }
