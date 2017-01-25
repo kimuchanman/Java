572a67dd9b1c9f3c7a5260a780422f18258777b6
Kei Kimura <kimuchan0811@gmail.com>
2017-01-25 11:59:16 +0900

    Add files via upload

---
+++ tanni.java
@@ -0,0 +1,44 @@
+package object;
+
+
+import java.util.Scanner;
+
+public class tanni {
+	public static void main(String[] args){
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.println("取得単位数をそれぞれ入力せよ");
+		System.out.println("文系の教養科目群：");
+		System.out.println("理系の教養科目群：");
+		System.out.println("基礎科目群：");
+		System.out.println("基礎ゼミナール：");
+		System.out.println("情報リテラシー実践");
+		System.out.println("言語科目");
+		System.out.println("保健体育科目");
+		System.out.println("理系共通基礎科目");
+
+		//switch
+		System.out.println("微分積分Ⅰは取っていますか？");
+		System.out.println("線形代数Ⅰは取っていますか？");
+		System.out.println("微分積分Ⅱは取っていますか？");
+		System.out.println("線形代数Ⅱは取っていますか？");
+		System.out.println("工学系電気回路は取っていますか？");
+
+		System.out.println("他の理系共通基礎科目の単位数：");
+
+		System.out.println("情報通信工学基礎科目");
+		System.out.println("情報工学分野科目");
+		System.out.println("通信工学分野科目");
+		System.out.println("情報通信応用分野科目");
+		System.out.println("情報通信システム演習科目");
+		System.out.println("実験科目");
+		System.out.println("特別研究");
+		System.out.println("学部共通科目");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+
+	}
+
+
+}
